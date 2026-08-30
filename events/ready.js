773c8a456de2513c0dc3c11931023d4720e836b9
hehechuppy <nguyenlehoangsang111@gmail.com>
2026-08-30 20:01:58 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -11,12 +11,12 @@ module.exports = {
 
         const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
         try {
-            console.log('Đang đăng ký lệnh gạch chéo (/)...');
+            console.log('Đang cập nhật danh sách lệnh gạch chéo (/)...');
             await rest.put(
                 Routes.applicationCommands(client.user.id),
                 { body: commandsData }
             );
-            console.log('Đăng ký lệnh thành công!');
+            console.log('Đăng ký lại danh sách Slash Command thành công!');
         } catch (error) {
             console.error(error);
         }
