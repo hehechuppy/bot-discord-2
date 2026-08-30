2fbcdb371e8dd2a2384f377d98ba5199318f1910
hehechuppy <nguyenlehoangsang111@gmail.com>
2026-08-30 19:54:07 +0700

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,26 @@
+const { REST, Routes } = require('discord.js');
+const { scheduleBirthdayCheck } = require('../utils/birthdayScheduler');
+
+module.exports = {
+    name: 'ready',
+    once: true,
+    async execute(client) {
+        console.log(`Bot đã đăng nhập thành công dưới tên: ${client.user.tag}`);
+
+        const commandsData = Array.from(client.commands.values()).map(cmd => cmd.data.toJSON());
+
+        const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+        try {
+            console.log('Đang đăng ký lệnh gạch chéo (/)...');
+            await rest.put(
+                Routes.applicationCommands(client.user.id),
+                { body: commandsData }
+            );
+            console.log('Đăng ký lệnh thành công!');
+        } catch (error) {
+            console.error(error);
+        }
+
+        scheduleBirthdayCheck(client);
+    }
+};
