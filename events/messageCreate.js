ed9896bab43c5048c2d46a50333bd9e12f4c8b04
hehechuppy <nguyenlehoangsang111@gmail.com>
2026-09-18 20:30:28 +0700

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -111,5 +111,6 @@ module.exports = {
         if (contentLower === 'xin chào') message.channel.send(`Chào ${message.author.username}! Mình có thể giúp gì cho bạn?`);
         if (contentLower === 'cảm ơn') message.channel.send(`Không có gì đâu ${message.author.username}!`);
         if (contentLower === 'ê') message.reply('ê cái gì mà ê');
+        if (contentLower === 'tick' message.reply('tạo ticket ở đây <#1501567490679898244>');
     }
 };
